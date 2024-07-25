17f853c8f98aae3769f9740a7d96e517da70ae74
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-07-25 01:46:11 -0700

    video 5 of BMI calculator, part 3 extract and refactor code ,shorten the code (lecture 38)
    in this video we will learn how to extract and refactor the code to make it shorter .
    we will refactor the container widget because it is used many places repeatedly.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -18,62 +19,49 @@ class _InputPageState extends State<InputPage> {
     child: Row(
         children: [
         Expanded(
-            child: Container(
-        margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1E3E), // Corrected color value
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-     ),
+            child: new RepeatContainerCode(Color(0xFF1D1E33)),
     ),
     Expanded(
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E3E), // Corrected color value
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
+      child: new RepeatContainerCode(Color(0xFF1D1E33)),
      ),
     ],
     ),
     ),
     Expanded(
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E3E), // Corrected color value
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
+      child: new RepeatContainerCode(Color(0xFF1D1E33)),
     ),
     Expanded(
     child: Row(
     children: [
     Expanded(
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E3E), // Corrected color value
-          borderRadius: BorderRadius.circular(10.0),
-        ),
+      child: new RepeatContainerCode(Color(0xFF1D1E33)),
       ),
-
-    ),
     Expanded(
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E3E), // Corrected color value
-          borderRadius: BorderRadius.circular(10.0),
+      child: new RepeatContainerCode(Color(0xFF1D1E33)),
               ),
-             ),
-            ),
            ],
          ),
         ),
        ],
       ),
     );
+  }
+}
+
+
+class RepeatContainerCode extends StatelessWidget {
+
+  RepeatContainerCode(this.colors);
+  final Color colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colors,
+        borderRadius: BorderRadius.circular(10.0),
+      ), // BoxDecoration
+    ); // Container
   }
 }
