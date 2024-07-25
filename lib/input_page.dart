d71eda0f45fb69c423bb148a0c976b21d5a434c0
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-07-25 04:03:44 -0700

    video 6 of BMI part2,Create icon and Text widgets (Lecture39)
    In this video we will learn how to create the icon and text widgets separately and use the extracting and refactoring on  both the icon and text widget to make the ode shorten

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -19,25 +21,50 @@ class _InputPageState extends State<InputPage> {
     child: Row(
         children: [
         Expanded(
-            child: new RepeatContainerCode(Color(0xFF1D1E33)),
+            child: new RepeatContainerCode(Color(0xFF1D1E33),
+                RepeatTextICONeWidget(
+                  FontAwesomeIcons.male,
+                  "MALE",
+                ),
+            ),
     ),
     Expanded(
-      child: new RepeatContainerCode(Color(0xFF1D1E33)),
+      child: new RepeatContainerCode(Color(0xFF1D1E33),
+    RepeatTextICONeWidget(
+     FontAwesomeIcons.female,
+     "FEMALE",
+      ),
      ),
+    ),
     ],
     ),
     ),
     Expanded(
-      child: new RepeatContainerCode(Color(0xFF1D1E33)),
+      child: new RepeatContainerCode(Color(0xFF1D1E33),
+        RepeatTextICONeWidget(
+          FontAwesomeIcons.male,
+          "MALE",
+        ),
+      ),
     ),
     Expanded(
     child: Row(
     children: [
     Expanded(
-      child: new RepeatContainerCode(Color(0xFF1D1E33)),
+      child: new RepeatContainerCode(Color(0xFF1D1E33),
+        RepeatTextICONeWidget(
+          FontAwesomeIcons.male,
+          "MALE",
+        ),
+      ),
       ),
     Expanded(
-      child: new RepeatContainerCode(Color(0xFF1D1E33)),
+      child: new RepeatContainerCode(Color(0xFF1D1E33),
+        RepeatTextICONeWidget(
+          FontAwesomeIcons.male,
+          "MALE",
+        ),
+      ),
               ),
            ],
          ),
@@ -49,14 +76,44 @@ class _InputPageState extends State<InputPage> {
 }
 
 
+class RepeatTextICONeWidget extends StatelessWidget {
+  RepeatTextICONeWidget(this.iconData,this.label);
+  final IconData iconData;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 85.0,
+        ), // Icon
+        SizedBox(
+          height: 15.0, // Corrected height
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
 class RepeatContainerCode extends StatelessWidget {
 
-  RepeatContainerCode(this.colors);
+  RepeatContainerCode(this.colors, this.cardWidget);
   final Color colors;
-
+  final Widget cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardWidget,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colors,
@@ -65,3 +122,4 @@ class RepeatContainerCode extends StatelessWidget {
     ); // Container
   }
 }
+
