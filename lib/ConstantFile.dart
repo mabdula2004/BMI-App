4f79eb0646b62b983091514a0f925c5dd3ab1c90
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-07-25 23:27:21 -0700

    video 16 BMI Complete 2nd Screen Lecture 50
    in this video we set the widget in the next screen and modify according to need

--- lib/ConstantFile.dart
+++ lib/ConstantFile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 const KLabelStyle=TextStyle(
@@ -14,5 +16,27 @@ fontWeight: FontWeight.w900,
 
 const KLargeButtonstyle=TextStyle(
   fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const KTitleStyle=TextStyle(
+  fontSize:25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const KResultStyle=TextStyle(
+color: Colors.green,
+fontSize: 18.0,
+fontWeight: FontWeight.bold,
+);
+
+const KBmiTextStyle=TextStyle(
+  color: Colors.white,
+  fontSize: 80.0,
+  fontWeight: FontWeight.w900,
+);
+const KBodyTextStyle=TextStyle(
+  color: Colors.white,
+  fontSize: 16.0,
   fontWeight: FontWeight.bold,
 );
